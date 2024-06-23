b3963dfa38910e5788bc727c580bea3a6171ae95
whoami <az.khabibullin2018@gmail.com>
2024-06-23 13:38:39 +0500

    урок формат json в списоке хтмл пользователь выбирает страну, выбранная страна отправляется на бэк, в бэке возвращается словарь городов, который отображается в другом списке для городов отправка на фронте через Ajax GET, апи тот же самый - костыльный, контроллер получает строку, возвращает словарь сериализованный в json

--- learn_ajax/Controllers/ValuesController.cs
+++ learn_ajax/Controllers/ValuesController.cs
@@ -5,20 +5,38 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using Newtonsoft.Json;
+
 namespace learn_ajax.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values/
-        public string Get(string Name, int Number)
-        {
-            return "Data: Name - " + Name + ", Number - " + Number;
-        }
-
         //GET api/values/
-        public string Get(string Name)
+        public string Get(string country)
         {
-            return Name == "admin"? "Fail": "Success";
+            var dictionary = new Dictionary<int, string>();
+
+            switch (country)
+            {
+                case "1":
+                    {
+                        dictionary.Add(1, "Paris");
+                        dictionary.Add(2, "Marseille");
+                        break;
+                    };
+                case "2":
+                    {
+                        dictionary.Add(1, "NY");
+                        dictionary.Add(2, "LA");
+                        break;
+                    };
+                default:
+                    {
+                        break;
+                    };
+            }
+
+            return JsonConvert.SerializeObject(dictionary, Formatting.Indented);
         }
 
         /*
